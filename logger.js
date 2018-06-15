c13397cc3a2d7620a31fa5ccdd68f248805d9671
MatthewSH <matthewh@halfpetal.com>
2018-06-15 04:00:17 -0400

    Adding database and onChatMessage functionality.

--- logger.js
+++ logger.js
@@ -1,19 +1,36 @@
 let api;
 let helper;
+let database;
 
 exports.constructor = (api, helper) => {
     this.api = api;
     this.helper = helper;
+    this.database = require("./database");
+    this.database.messages.sync();
 };
 
 exports.onEraseMessage = {
     execute: (data, IErasedMessage) => {
-
     }
 };
 
 exports.onChatMessage = {
     execute: (data, IChatMessage) => {
-
+        this.database.messages.findOrCreate({
+            where: {
+                id: IChatMessage.messageId
+            },
+            defaults: {
+                id: IChatMessage.messageId,
+                roomId: IChatMessage.roomId,
+                userId: IChatMessage.userId,
+                name: IChatMessage.username,
+                message: IChatMessage.message
+            }
+        }).spread((message, created) => {
+            if(process.env.MESSAGE_LOGGER_OUTPUT_MESSAGES) {
+                console.log(message.name + ": " + message.message);
+            }
+        });
     }
 };
